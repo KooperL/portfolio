ebb847b4b159e94a6ddaf135a13fd63ca26764ab
KooperL <personal.p.github-23545@fe22e657.com>
2022-09-16 22:59:29 +1000

    replaced self-made  graphics engine with p5.js :(

---
+++ ui.react/src/components/p5/box.tsx
@@ -0,0 +1,55 @@
+import { P5Instance } from "react-p5-wrapper";
+import { makeNoise3D } from "open-simplex-noise";
+import { Noise3D } from "open-simplex-noise/lib/3d";
+
+
+function sketchWrapper(width: number, height: number, background: string, stroke: string) {
+  function sketch(p5: P5Instance) {
+    const scale = 50;
+    const w = 600;
+    const h = 600;
+    const cols = width/scale;
+    const rows = height/scale;
+
+    const speedModifier = 100;
+    const heightModifier = 9;
+    const magnifier = 20;
+    let noise: Noise3D;
+
+    p5.setup = () => {
+      p5.createCanvas(width, height, p5.WEBGL);
+      noise = makeNoise3D(Date.now());
+      p5.frameRate(60)
+    };
+
+    p5.draw = () => {
+      p5.background(background);
+      // p5.normalMaterial();
+      // p5.push();
+      // p5.rotateZ(p5.frameCount * 0.01);
+      // p5.rotateX(p5.frameCount * 0.01);
+      // p5.rotateY(p5.frameCount * 0.01);
+      // p5.plane(50);
+      // p5.pop();
+      // p5.translate(p5.width/2, p5.height/2);
+      p5.rotateX(p5.PI / 3);
+      p5.translate(p5.width/8, -p5.height, -300);
+      p5.rotateZ(p5.PI/3);
+      p5.stroke(stroke);
+      p5.strokeWeight(3);
+      p5.noFill();
+      for(let y = 0; y < rows-1; y++) {
+        p5.beginShape(p5.TRIANGLE_STRIP);
+        for(let x = 0; x < cols; x++) {
+          p5.vertex(x*scale, y*scale, (noise((x*scale/rows)/magnifier, (y*scale/cols)/magnifier, p5.frameCount/speedModifier)+1)**heightModifier);
+          p5.vertex(x*scale, (y+1)*scale, (noise((x*scale/rows)/magnifier, (y+1)*scale/cols/magnifier, p5.frameCount/speedModifier)+1)**heightModifier);
+        }
+        p5.endShape()
+      }
+    };
+  }
+  return sketch
+}
+
+
+export default sketchWrapper;
---
+++ ui.react/todo/renderer/renderer v1/grid/Render.tsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import { Dims, RenderProps } from "../utils/types";
+import { } from '../utils/math';
+import { drawBlueprint, DrawFigure } from "../Draw";
+import { Grid } from "../utils/raster";
+
+// todo
+export function Render(props: RenderProps) {
+  const w = props.dims.w ?? 1280;
+  const h = props.dims.h ?? 720;
+  const fov = props.dims.fov ?? 90;
+  const name = props.name ?? 'render';
+  return (
+    <div className="pt-20">
+      <DrawFigure name={name} w={w} h={h} />
+      <DrawSVG passed={props} />
+    </div>
+  );
+}
+
+function DrawSVG(props: {passed: RenderProps}) {
+  const [ticker, setTicker] = useState(0);
+  const [obj, setObj] = useState<Grid | null>(null);
+  const move = {
+    transformations: props.passed.transformations ?? {roll: 0.4, pitch: 4.7, yaw: 0.4},
+    translations: props.passed.translations ?? {x: 11, y: 40, z: 50}
+  }
+  // const [mouseSpeed, setMouseSpeed] = useState(0)
+
+  useEffect(() => {
+    const dims: Dims = {w: props.passed.dims.w, h: props.passed.dims.h, fov: props.passed.dims.fov}
+
+    const grid = new Grid(dims, props.passed.colours, 50, 50, 4);
+    // grid.noiseWarpZ(ticker, mouseSpeed, 6);
+
+    setObj(grid);
+
+    console.log(obj)
+  }, []);
+
+  // var totalX = 0;
+  // var totalY = 0;
+  // var moveX = 0;
+  // var moveY = 0;
+  
+  // document.addEventListener("mousemove", function(ev){
+  //     totalX += Math.abs(ev.movementX);
+  //     totalY += Math.abs(ev.movementY);
+  //     moveX += ev.movementX;
+  //     moveY += ev.movementY;
+  // }, false);
+  
+  // setInterval(function(){
+  //     // console.log(`Speed X: ${totalX}px/s, Y: ${totalY}px/s`);
+  //     // console.log(`Movement X: ${moveX}px/s, Y: ${moveY}px/s`);
+  //   setMouseSpeed(moveX+moveY)
+  //     moveX = moveY = totalX = totalY = 0;
+  // }, 10000);
+
+
+  useEffect(() => {
+    if(obj) {
+      obj.simpleRaster(
+        move.transformations,
+        move.translations
+      );
+      obj.noiseWarpZ(ticker, ticker*3, 6);
+      obj.orderTris()
+      // console.log(ticker);
+      // console.log(mouseSpeed)
+  }
+    setTimeout(() => setTicker(ticker+0.01),50);
+  }, [ticker]);
+
+  if(obj == null) {
+    return (
+      <p>Loading</p>
+    );
+  } else {
+    return drawBlueprint({'name': props.passed.name, 'tris': obj.trisToRaster});
+  }
+}
