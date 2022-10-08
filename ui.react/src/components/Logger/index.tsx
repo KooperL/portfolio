6704da39157e05da901adfa7d5a16d552a8a0ef5
KooperL <personal.p.github-23545@fe22e657.com>
2022-10-08 12:25:16 +1100

    Added basic and bearer auth api calls for blog project

--- ui.react/src/components/Logger/index.tsx
+++ ui.react/src/components/Logger/index.tsx
@@ -1,39 +1,69 @@
-
 import { useContext, useEffect, useState } from 'react';
 import { postCapture, postMonitor } from '../../containers/App/api/loggerApi';
-import { CapturePOSTPayload, MonitorPOSTPayload } from './types';
+import { LoggingPOSTResponse } from './types';
 
 
+interface Props {
+  capturePost: Function; 
+  monitorPost: Function; 
+}
 
-
-
-  interface Props {
-    capturePost: Function; 
-    monitorPost: Function; 
+function generateUniqueToken(length: number) {
+  let token = '';
+  let validChars = 'abcdefghijklmnopqrstuvwxyz'
+  for(let i=0;i<length;i++) {
+    let random = Math.random();
+    if(random>0.8) {
+      token = token.concat(parseInt((random*10).toString()).toString());
+    } else {
+      const index = Math.floor(Math.random() * validChars.length);
+      let alpha = validChars[index];
+      token = token.concat(index%2?alpha.toLowerCase():alpha.toUpperCase());
+    }
   }
+  return token
+}
 
 function Logger(props: Props) {
   useEffect(() => {
+    // TODO hash all three items
     if(!localStorage.getItem('uuid')) {
-      let uuid = '';
-      let validChars = 'abcdefghijklmnopqrstuvwxyz'
-      for(let i=0;i<20;i++) {
-        let random = Math.random();
-        if(random>0.8) {
-          uuid = uuid.concat(parseInt((random*10).toString()).toString());
-        } else {
-          const index = Math.floor(Math.random() * validChars.length);
-          let alpha = validChars[index];
-          uuid = uuid.concat(index%2?alpha.toLowerCase():alpha.toUpperCase());
-        }
-      }
-      localStorage.setItem('uuid', uuid);
+      localStorage.setItem('uuid', generateUniqueToken(10));
     }
-    // const isDarkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches   //todo
-    
+    if(!sessionStorage.getItem('session_id')) {
+      sessionStorage.setItem('session_id', generateUniqueToken(20));
+    }
+
     const canvasHash = document.getElementById('canvas-hash')?.innerText ?? '';
     if((!localStorage.getItem('canvas-hash')) || (localStorage.getItem('canvas-hash') !== canvasHash)) {
       localStorage.setItem('canvas-hash', canvasHash?.toString());
+      // TODO
+      // @ts-ignore 
+      const [browserVar, versionVar] = (() => {
+        const { userAgent } = navigator
+        let match = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []
+        let temp
+        if (/trident/i.test(match[1])) {
+          temp = /\brv[ :]+(\d+)/g.exec(userAgent) || []
+          return `IE ${temp[1] || ''}`
+        }
+        if (match[1] === 'Chrome') {
+          temp = userAgent.match(/\b(OPR|Edge)\/(\d+)/)
+          if (temp !== null) {
+            return temp.slice(1).join(' ').replace('OPR', 'Opera')
+          }
+          temp = userAgent.match(/\b(Edg)\/(\d+)/)
+          if (temp !== null) {
+            return temp.slice(1).join(' ').replace('Edg', 'Edge (Chromium)')
+          }
+        }
+        match = match[2] ? [ match[1], match[2] ] : [ navigator.appName, navigator.appVersion, '-?' ]
+        temp = userAgent.match(/version\/(\d+)/i)
+        if (temp !== null) {
+          match.splice(1, 1, temp[1])
+        }
+        return match
+      })()
       props.capturePost({
         canvas_hash: document.getElementById('canvas-hash')?.innerText,
         uuid: localStorage.getItem('uuid'),
@@ -57,14 +87,16 @@ function Logger(props: Props) {
           pixelDepth: +window.screen.pixelDepth ??  123,
           // },
           // navigator: {
-          platform: navigator.platform ?? null,
-          cookieEnabled: navigator.cookieEnabled ?? false,
+          platform: navigator.platform ?? 'null',
+          cookieEnabled: +navigator.cookieEnabled ?? 2,
           // 'java': navigator.javaEnabled ?? null,
           // 'online': navigator.onLine ?? null,
           // },
         // }
-        darkMode: window.matchMedia("(prefers-color-scheme: dark)").matches ?? false
-      }).then((resp: CapturePOSTPayload) => {
+        darkMode: +window.matchMedia("(prefers-color-scheme: dark)").matches ?? 2,
+        browser: browserVar ?? 'null',
+        version: versionVar ?? 'null'
+      }).then((resp: LoggingPOSTResponse) => {
         if(resp.success) {
         } else {
           throw new Error(resp.error);
@@ -75,9 +107,10 @@ function Logger(props: Props) {
     }
     props.monitorPost({
       uuid: localStorage.getItem('uuid'),
+      session_id: sessionStorage.getItem('session_id'),
       page: window.location.pathname,
       ...(localStorage.getItem('currentPage') && {prevPage: localStorage.getItem('currentPage')})
-    }).then((resp: MonitorPOSTPayload) => {
+    }).then((resp: LoggingPOSTResponse) => {
       if(resp.success) {
         localStorage.setItem('currentPage', window.location.pathname)
       } else {
