1d28fc25af079d65fac953f178f3e2d5268e582d
KooperL <personal.p.github-23545@fe22e657.com>
2022-12-30 17:09:11 +1100

    Create util functions for secondary route controller

--- server/v2.golang/go.mod
+++ server/v2.golang/go.mod
@@ -1,3 +1,5 @@
 module kooperlingohr/portfolio
 
 go 1.19
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- server/v2.golang/router/routes/RandomBio.go
+++ server/v2.golang/router/routes/RandomBio.go
@@ -1,19 +1,3 @@
-// package routes
-
-// import (
-// 	"fmt"
-// 	"net/http"
-// )
-
-// func Hello(w http.ResponseWriter, r *http.Request) {
-// 	word := r.URL.Query().Get("word")
-// 	if word == "banana" {
-// 		fmt.Fprintln(w, "Match found")
-// 	} else {
-// 		panic(fmt.Errorf("error message"))
-// 	}
-// }
-
 package routes
 
 import (
