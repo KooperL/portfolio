9361a6cc5e5ba1ecce18f9ca0142c8079115b16d
KooperL <kooper.lingohr@gmail.com>
2023-04-21 21:43:36 +1000

    Work on rust secondary route

--- server/v3.rust/src/main.rs
+++ server/v3.rust/src/main.rs
@@ -7,7 +7,7 @@ use serde::{Serialize, Deserialize};
 use std::fs;
 use std::collections::HashMap;
 //use serde_json::{Value};
-
+use std::ops::Index;
 
 #[derive(Serialize, Deserialize)]
 struct ToFromValue {
@@ -54,6 +54,19 @@ struct Propensities {
     turn: f32,
 }
 
+impl Index<&str> for Propensities {
+    type Output = f32;
+
+    fn index(&self, key: &str) -> &Self::Output {
+        match key {
+            "alpha_helix" => &self.alpha_helix,
+            "beta_strand" => &self.beta_strand,
+            "turn" => &self.turn,
+            _ => panic!("Invalid key: {}", key),
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize)]
 struct SideChain {
     class: String,
@@ -66,21 +79,45 @@ struct AminoAcid {
     name: String,
     three_letter_symbol: String,
     symbol: String,
-    side_chain: side_chain,
+    side_chain: SideChain,
     nucleotides: Vec<String>,
     hydropathy_index: f32,
     molecular_weight: f32,
-    propensities: propensities,
+    propensities: Propensities,
 }
 
-fn find_nucleation_region(arr: Vec<f32>, threshold: f32, sliding_window: usize, contiguous_window: f32) -> Vec<f32> {
-    let mut nucleation_regions:Vec<f32> = Vec::new();
+fn find_nucleation_region(arr: Vec<f32>, threshold: f32, sliding_window: usize, contiguous_window: usize, sError: usize) -> Vec<bool> {
+    let mut returnArrLen = arr.len() - sliding_window - 1;
+    let mut nucleation_regions = vec![false; returnArrLen];
+
+    let mut validNucleationLength = 0;
     let mut start = 0;
     let mut end = sliding_window;
+
     while end <= arr.len() {
         let slice = &arr[start..sliding_window];
-        let slice_sum = slice.iter().sum();
-        nucleation_regions.push(slice_sum);
+        let slice_sum: f32 = slice.iter().sum();
+        // nucleation_regions.push(slice_sum);
+        let isValid: bool = slice_sum > (contiguous_window as f32 * threshold);
+
+        if isValid {
+            validNucleationLength+=1;
+        } else {
+            validNucleationLength-=1;
+        };
+
+		if validNucleationLength > contiguous_window {
+			validNucleationLength -= 1;
+			for i in start..end {
+				nucleation_regions[i] = true;
+			}
+		} else {
+			if validNucleationLength > sError {
+				nucleation_regions[start] = true;
+			} else {
+				nucleation_regions[start] = false;
+			}
+		}
         start += 1;
         end += 1;
     }
@@ -91,7 +128,7 @@ fn populate_propensities_from_symbol<'a>(
     symbol: &str,
     key: &str,
     memo: &'a HashMap<String, HashMap<String, f32>>,
-    file: &[AminoAcid],
+    file: &Vec<AminoAcid>,
 ) -> Option<&'a f32> {
     let inner_map = memo.get(key)?;
     if let Some(value) = inner_map.get(symbol) {
@@ -110,31 +147,69 @@ fn populate_propensities_from_symbol<'a>(
 }
 
 #[get("/projects/secondary?<aas>&<aaformat>&<threshold>&<avg>")]
-pub async fn secondary(aas: String, aaformat: String, threshold: i8, avg: i8) -> Result<Json<GenericResponse>, Status> {
+pub async fn secondary(aas: String, aaformat: String, threshold: i32, avg: i32) -> Result<Json<GenericResponse>, Status> {
     let file_contents = fs::read_to_string("../data/aminoAcids.json").expect("File should have been opened");
     //let parsed_file = serde::Deserializer(file_contents);
     let parsed_file: Vec<AminoAcid> = serde_json::from_str(&file_contents).unwrap();
 
     let alpha_helix_threshold = 1.03;
     let alpha_helix_sliding_window = 6;
-	let alpha_helix_contiguous_window = 4.0;
-    let alpha_helix_error = 6.0;
+	let alpha_helix_contiguous_window = 4;
+    let alpha_helix_error = 6;
     let	beta_sheet_threshold = 1.0;
 	let beta_sheet_sliding_window = 5;
-    let beta_sheet_contiguous_window = 3.0;
-	let beta_sheet_error = 4.0;
+    let beta_sheet_contiguous_window = 3;
+	let beta_sheet_error = 4;
 
     let mut memo = HashMap::new();
     let mut alpha_helix_map = HashMap::new();
     let mut beta_strand_map = HashMap::new();
-    memo.insert("alpha_helix", alpha_helix_map); 
-    memo.insert("beta_strand", beta_strand_map);
+    memo.insert(String::from("alpha_helix"), alpha_helix_map); 
+    memo.insert(String::from("beta_strand"), beta_strand_map);
 
-    let hPropensities: Vec<&f32> = Vec::new();
+    let mut hPropensities: Vec<f32> = Vec::new();
+    let mut ePropensities: Vec<f32> = Vec::new();
     for amino_acid in aas.chars() {
-        let prop = populate_propensities_from_symbol(amino_acid, "alpha_helix", &memo, &parsed_file).unwrap();
-        hPropensities.push(prop);
+        let proph = populate_propensities_from_symbol(&amino_acid.to_string(), "alpha_helix", &memo, &parsed_file).unwrap();
+        hPropensities.push(*proph);
+        let prope = populate_propensities_from_symbol(&amino_acid.to_string(), "beta_strand", &memo, &parsed_file).unwrap();
+        ePropensities.push(*prope);
     };
+    
+    let hNucleationRegions = find_nucleation_region(hPropensities, alpha_helix_threshold, alpha_helix_sliding_window, alpha_helix_contiguous_window, alpha_helix_error);
+    let eNucleationRegions = find_nucleation_region(ePropensities, beta_sheet_threshold, beta_sheet_sliding_window, beta_sheet_contiguous_window, beta_sheet_error);
+
+    let mut result = Vec::new();
+    let mut stalemate_slice = Vec::new();
+    for ind in 0..aas.len() {
+        if hNucleationRegions[ind] && !eNucleationRegions[ind] {
+            result[ind] = "h";
+            if stalemate_slice.len() > 0 {
+                for i in stalemate_slice {
+                    result[i] = "h";
+                };
+            };
+            stalemate_slice = Vec::new();
+        } else if !hNucleationRegions[ind] && eNucleationRegions[ind] {
+            result[ind] = "e";
+            if stalemate_slice.len() > 0 {
+                for i in stalemate_slice {
+                    result[i] = "e";
+                };
+            };
+            stalemate_slice = Vec::new();
+        } else if !hNucleationRegions[ind] && !eNucleationRegions[ind] {
+            result[ind] = "c";
+        } else {
+            result[ind] = "_";
+            stalemate_slice.push(ind);
+        };
+    };
+        Ok(Json(GenericResponse {
+        success: true,
+        data: Some(result.join("")),
+            errorMessage: None,
+    }))
     
 }
 
@@ -250,5 +325,5 @@ pub async fn randomBio(typeTemp: i8, length: i16) -> Result<Json<GenericResponse
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![randomBio, seqAlign])
+    rocket::build().mount("/", routes![randomBio, seqAlign, secondary])
 }
