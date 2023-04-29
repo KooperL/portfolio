7033fdcb1c9e111922a74678b8f43c200edb7d6f
KooperL <kooper.lingohr@gmail.com>
2023-04-29 21:00:26 +1000

    Add contact post route in `v3.rust`

--- server/v3.rust/src/routes/index/contact.rs
+++ server/v3.rust/src/routes/index/contact.rs
@@ -1,8 +1,13 @@
-use rocket::{get, http::Status, serde::json::Json};
-use std::fs;
+use rocket::{get, post, http::Status, serde::json::Json};
+use std::{fs};
+use sqlx::{migrate::MigrateDatabase, Row, Sqlite, SqlitePool};
 //use std::{str,fs};
 #[path = "../../types/response.rs"] mod response;
 #[path = "../../types/jsonContent.rs"] mod json_content;
+#[path = "../../utils/discordLogging.rs"] mod discordLogging;
+// #[path = "../../utils/diesel.rs"] mod database;
+// #[path = "../../diesel/schema/insertables.rs"] mod insertables;
+// #[path = "../../diesel/schema/models.rs"] mod models;
 use std::collections::HashMap;
 
 
@@ -13,6 +18,39 @@ pub async fn contactRouteGet() -> Result<Json<response::GenericResponse<Vec<json
     Ok(Json(response::GenericResponse {
         success: true,
         data: Some(parsed_file),
+        errorMessage: None,
+    }))
+}
+
+
+struct contact_message {
+    session_id: String,
+    message: String,
+}
+
+#[post("/contact", data = "<data>")]                                                                            
+pub async fn contactRoutePost(data: String) -> Result<Json<response::GenericResponse<String>>, Status> {
+		discordLogging::discord_post(format!("{data}"));
+	//	discordLogging::discord_post(format!("{a},\n{b}", a=input.session_id, b=input.message));
+	let insertStatement = "INSERT INTO contact_messages VALUES (?, ?, ?, ?);";
+
+    // let connection = database::establish_connection();
+    // diesel::insert_into(&models::ContactMessage).values(insertables::NewPost {
+    //     date: "123",
+    //     session_id: "123",
+    //     message: "123",
+    // }).execute(&connection).expect("Error inserting into database");
+    
+    const DB_URL: &str = "sqlite://server/data/database.db";
+    let db = SqlitePool::connect(DB_URL).await.unwrap();
+    let time = format!("{}", chrono::Local::now().format("%Y-%m-%d %H:%M:%S"));
+    let result = sqlx::query("INSERT INTO contact_messages VALUES (NULL, ?, ?, ?);").bind(time).bind(&data).bind(&data)
+    .execute(&db)
+    .await
+    .unwrap();
+    Ok(Json(response::GenericResponse {
+        success: true,
+        data: Some(String::from("parsed_file")),
         errorMessage: None,
     }))
 }
