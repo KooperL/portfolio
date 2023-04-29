d61534cef2756024561a76f3b3579c41ea298a6f
KooperL <personal.p.github-23545@fe22e657.com>
2023-04-29 22:25:11 +1000

    Updated `db` routing and stucts

--- server/v3.rust/src/main.rs
+++ server/v3.rust/src/main.rs
@@ -6,15 +6,12 @@ use sqlx::{migrate::MigrateDatabase, Row, Sqlite, SqlitePool};
 #[path = "routes/projects/randombio.rs"] mod randombio;
 #[path = "routes/projects/mrna.rs"] mod mrna;
 #[path = "routes/projects/index.rs"] mod projectsIndex;
+#[path = "routes/projects/fuelprices.rs"] mod fuelprices;
 #[path = "routes/forum/index.rs"] mod forumIndex;
 #[path = "routes/index/index.rs"] mod index;
 #[path = "routes/index/contact.rs"] mod contact;
 #[path = "routes/index/about.rs"] mod about;
 #[path = "utils/dotenv.rs"] mod dotenv;
-
-use std::fs::File;
-
-
 
 #[launch]
 async fn rocket() -> _ {
@@ -33,20 +30,9 @@ async fn rocket() -> _ {
     } else {
         println!("Database already exists");
     }
-    let db = SqlitePool::connect(DB_URL).await.unwrap();
-    let result = sqlx::query(
-        "select uuid from fingerprint;",
-    )
-    .fetch_all(&db)
-    .await
-    .unwrap();
-
-    for (idx, row) in result.iter().enumerate() {
-        println!("[{}]: {:?}", idx, row.get::<String, &str>("uuid"));
-    }
 
     rocket::build().mount("/", routes![
-        secondary::secondaryRouteGet, seqalign::seqAlignRouteGet, randombio::randomBioRouteGet, mrna::mrnaRouteGet, projectsIndex::projectIndexRouteGet,
+        secondary::secondaryRouteGet, seqalign::seqAlignRouteGet, randombio::randomBioRouteGet, mrna::mrnaRouteGet, projectsIndex::projectIndexRouteGet, fuelprices::fuelpricesRouteGet,
         index::indexRouteGet, contact::contactRouteGet, about::aboutRouteGet,
         forumIndex::forumIndexRouteGet
     ])
