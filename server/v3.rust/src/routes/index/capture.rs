429daa3b1a2435304ebd2dfb983aa2e49a8b8cfc
KooperL <personal.p.github-23545@fe22e657.com>
2023-05-05 23:19:50 +1000

    Housekeeping

--- server/v3.rust/src/routes/index/capture.rs
+++ server/v3.rust/src/routes/index/capture.rs
@@ -5,36 +5,87 @@ use sqlx::{migrate::MigrateDatabase, Row, Sqlite, SqlitePool};
 #[path = "../../types/response.rs"] mod response;
 
 #[derive(Deserialize)]
-struct CapturePayload {
-	uuid: String,
-	canvas_hash: String,
-	version: String,
-	platform: String,
-	browser: String,
-	darkMode: String,
-	cookieEnabled: String,
-	actualHeight: String,
-	actualWidth: String,
-	pixelDepth: String,
-	innerHeight: String,
-	outerHeight: String,
-	innerWidth: String,
-	outerWidth: String,
+pub struct CapturePayload {
+	pub uuid: String,
+	pub canvas_hash: String,
+	pub version: String,
+	pub platform: String,
+	pub browser: String,
+	pub darkMode: String,
+	pub cookieEnabled: String,
+	pub actualHeight: String,
+	pub actualWidth: String,
+	pub pixelDepth: String,
+	pub innerHeight: String,
+	pub outerHeight: String,
+	pub innerWidth: String,
+	pub outerWidth: String,
 }
 
-#[post("/capture", data = "<CapturePayload>")]                                                                            
-pub async fn captureRoutePost(input: CapturePayload) -> Result<Json<response::GenericResponse<String>>, Status> {
+// impl<'a, 'r> FromRequest<'a, 'r> for UserAgent {
+//     type Error = std::convert::Infallible;
+// 
+//     fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
+//         let user_agent = request.headers().get_one("User-Agent");
+//         match user_agent {
+//           Some(user_agent) => {
+//             Outcome::Success(UserAgent(user_agent.to_string()))
+//           },
+//           None => Outcome::Failure(Status::Unauthorized)
+//         }
+//     }
+// }
+
+#[derive(Debug)]
+pub struct ClientIp(String);
+
+#[rocket::async_trait]
+impl<'r> request::FromRequest<'r> for ClientIp {
+    type Error = ();
+
+    async fn from_request(request: &'r request::Request<'_>) -> request::Outcome<Self, Self::Error> {
+        request.headers().get_one("X-Forwarded-For")
+            // .and_then(|x| x.split(',').next())
+            // .or_else(|| request.remote().map(|r| r.ip().to_string().trim()))
+            // .map(ClientIp)
+            // .map(request::Outcome::Success)
+            // .unwrap_or_else(|| request::Outcome::Failure((Status::BadRequest, ())))
+            .map(|ip| ClientIp(ip.to_string()))
+            .map(request::Outcome::Success)
+            .unwrap_or_else(|| request::Outcome::Failure((Status::BadRequest, ())))
+    }
+}
+
+#[derive(Debug)]
+pub struct UserAgent(String);
+
+#[rocket::async_trait]
+impl<'r> request::FromRequest<'r> for UserAgent {
+    type Error = ();
+
+    async fn from_request(request: &'r request::Request<'_>) -> request::Outcome<Self, Self::Error> {
+        request.headers().get_one("User-Agent")
+            .map(|ua| UserAgent(ua.to_string()))
+            .map(request::Outcome::Success)
+            .unwrap_or_else(|| request::Outcome::Failure((Status::BadRequest, ())))
+    }
+}
+
+#[post("/capture", data = "<input>")]                                                                            
+pub async fn captureRoutePost(input: Json<CapturePayload>, user_agent: UserAgent, client_ip: ClientIp) -> Result<Json<response::GenericResponse<String>>, Status> {
+    const DB_URL: &str = "sqlite://server/data/database.db";
+    let db = SqlitePool::connect(DB_URL).await.unwrap();
     let time = format!("{}", chrono::Local::now().format("%Y-%m-%d %H:%M:%S"));
     let insertStatement = "INSERT INTO fingerprint VALUES (
 		  NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
 		);";
-    let result_monitor = sqlx::query(insertStatementMonitor)
+    let result_monitor = sqlx::query(insertStatement)
         .bind(&input.uuid)
         .bind(&input.canvas_hash)
         .bind(&input.platform)
         .bind(&input.browser)
         .bind(&input.version)
-        .bind(r.Header.Get("User-Agent"))
+        .bind(user_agent.0)
         .bind(&input.darkMode)
         .bind(&input.cookieEnabled)
         .bind(&input.actualHeight)
@@ -44,7 +95,7 @@ pub async fn captureRoutePost(input: CapturePayload) -> Result<Json<response::Ge
         .bind(&input.innerWidth)
         .bind(&input.outerHeight)
         .bind(&input.outerWidth)
-        .bind(The IP address),
+        .bind(client_ip.0)
     .execute(&db)
     .await
     .unwrap();
--- server/v3.rust/src/routes/index/monitor.rs
+++ server/v3.rust/src/routes/index/monitor.rs
@@ -8,22 +8,28 @@ use sqlx::{migrate::MigrateDatabase, Row, Sqlite, SqlitePool};
 struct MonitorPayload {
 	session_id: String, 
 	uuid: String,
-	page: Stirng,
+	page: String,
 	prevPage: Option<String>,
 }
 
-#[post("/monitor", data = "<MonitorPayload>")]                                                                            
-pub async fn monitorRoutePost(input: MonitorPayload) -> Result<Json<response::GenericResponse<String>>, Status> {
+#[post("/monitor", data="<input>")]                                                                            
+pub async fn monitorRoutePost(input: Json<MonitorPayload>) -> Result<Json<response::GenericResponse<String>>, Status> {
+    const DB_URL: &str = "sqlite://server/data/database.db";
+    let db = SqlitePool::connect(DB_URL).await.unwrap();
     let time = format!("{}", chrono::Local::now().format("%Y-%m-%d %H:%M:%S"));
 	let insertStatementMonitor = "INSERT INTO monitor VALUES (NULL, ?, ?, ?, ?);";
-    let result_monitor = sqlx::query(insertStatementMonitor).bind(time).bind(&input.uuid).bind(&input.session_id).bind(&input.page)
+    let result_monitor = sqlx::query(insertStatementMonitor)
+        .bind(&time)
+        .bind(&input.uuid)
+        .bind(&input.session_id)
+        .bind(&input.page)
     .execute(&db)
     .await
     .unwrap();
 
-	if let Some(e) = input.prevPage {
+	if let Some(e) = &input.prevPage {
 		let insertStatementTrack = "INSERT INTO route_track VALUES (NULL, ?, ?, ?, ?);";
-        let result_track = sqlx::query(insertStatementTrack).bind(time).bind(&input.session_id).bind(&input.prevPage).bind(&input.page)
+        let result_track = sqlx::query(insertStatementTrack).bind(&time).bind(&input.session_id).bind(&input.prevPage).bind(&input.page)
         .execute(&db)
         .await
         .unwrap();
