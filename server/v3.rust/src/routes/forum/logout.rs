a7bc069512d73c770bf8a484dc96a6e9938add0e
KooperL <kooper.lingohr@gmail.com>
2023-05-25 08:37:49 +1000

    Update logic in `forum` routes

--- server/v3.rust/src/routes/forum/logout.rs
+++ server/v3.rust/src/routes/forum/logout.rs
@@ -1,4 +1,4 @@
-use rocket::{get, post, http::Status, serde::json::Json, request};
+use rocket::{get, post, http::Status, serde::json::Json, request, futures::StreamExt};
 use serde::Deserialize;
 use std::{fs, io::Read};
 use sqlx::{migrate::MigrateDatabase, Row, Sqlite, SqlitePool};
@@ -6,44 +6,73 @@ use rand::Rng;
 use base64::{Engine as _, engine::general_purpose};
 #[path = "../../types/response.rs"] mod response;
 #[path = "../../types/auth.rs"] mod auth;
-use ring::{digest, pbkdf2};
 // use std::sync::{Arc, Mutex};
 use tokio::sync::{Mutex};
 
+use ring::{digest, pbkdf2, hmac};
+use chrono::{Local, Duration};
+
+#[derive(Debug, sqlx::FromRow)]
+struct refresh_token_row {
+    id: i64,
+    date: String,
+    forum_referesh_token: String,
+    count: i64
+}
 
 pub struct bearer_token {
     header: auth::jwt_header,
     body: auth::jwt_access_token_body,
-    signature: String
+    // signature: String
 }
 
 #[rocket::async_trait]
 impl<'r> request::FromRequest<'r> for bearer_token {
     type Error = ();
     async fn from_request(request: &'r request::Request<'_>) -> request::Outcome<Self, Self::Error> {
-        let header_body = request.headers().get_one("Authorization");
-            header_body.map(|header_text| {
+        let verified_token = request.headers().get_one("Authorization")
+            .map(|header_text| {
                 let encoded = general_purpose::STANDARD_NO_PAD.decode(header_text).unwrap();
                 let decoded = String::from_utf8(encoded).unwrap();
                 let split_header: Vec<&str> = decoded.split(" ").collect();
                 let auth_type: String = String::from(split_header[0]);
 
                 if auth_type != "Bearer" {
-                    // Throw 401
+                    return request::Outcome::Failure((Status::Unauthorized, ()));
                 }
 
                 let auth_token: String = String::from(split_header[1]);
                 let split_token: Vec<&str> = decoded.split(".").collect();
-                serde_json::parse::<jwt_header>(split_token[0]);
-                let parsed_header = jwt_header {
+                let parsed_body: auth::jwt_access_token_body = serde_json::from_str(split_token[0]).unwrap();
+                let parsed_header: auth::jwt_header = serde_json::from_str(split_token[1]).unwrap();
+                let signature_external = split_token[2].as_bytes();
+
+                let access_token_signing_string = dotenvy::var("forum-jwt-auth-token").expect("forum-jwt-auth-token must be set");
+                let access_token_hmac_key = hmac::Key::new(hmac::HMAC_SHA256, access_token_signing_string.as_bytes());
+                let string_of_token =  format!("{head}.{body}",
+                    head = serde_json::to_string(&parsed_header).unwrap(), 
+                    body = serde_json::to_string(&parsed_body).unwrap()
+                ).as_bytes();
+                let signature_internal = hmac::sign(&access_token_hmac_key, string_of_token);
+
+                // TODO: if jwt_header.typ is tampered? Should be tracked in db
+                let tokens_match = hmac::verify(&access_token_hmac_key, string_of_token, signature_external);
+                if tokens_match.is_err() {
+
+                    return request::Outcome::Failure((Status::Unauthorized, ()));
                 };
-                let parsed_body = jwt_access_token_body {
-                    Username: username,
-                    Password: password
-                };
+                if parsed_body.expires > Local::now().timestamp() {
+                    return request::Outcome::Failure((Status::Unauthorized, ()));
+                }
+                
+                return request::Outcome::Success((Status::Ok, bearer_token {
+                    header: parsed_header, 
+                    body: parsed_body
+                    // signature: String::from_utf8(signature_internal.).unwrap()
+                }));
             })
             .map(request::Outcome::Success)
-            .unwrap_or_else(|| request::Outcome::Failure((Status::BadRequest, ())))
+            .unwrap_or_else(|| request::Outcome::Failure((Status::BadRequest, ())));
     }
 }
 
@@ -56,59 +85,45 @@ pub async fn logoutRoutePost(auth_header: bearer_token) -> Result<Json<response:
         .max_connections(5)
         .connect("sqlite://server/data/database.db?mode=rwc").await.unwrap();
     let time = format!("{}", chrono::Local::now().format("%Y-%m-%d %H:%M:%S"));
-    let forumUserExistsQuery = "
+    let forumUserIsLoggedInQuery = "
       SELECT 
-       id 
-      from forum_users
+        id,
+        date,
+        forum_referesh_token,
+        count(forum_user_id) as count
+      from forum_refresh_tokens 
       where
-        lower(forum_username) = lower(?)
-      limit 1;
+        forum_user_id = ?;
     ";
-    //let mut tx = pool.begin().await.expect("begin tx");
-    let userExists = sqlx::query(forumUserExistsQuery)
-        .bind(&auth_header.Username)
-        //.execute(&db.acquire())
-        .execute(&pool)
-        .await
-        .unwrap();
-    // db.close();
-    
-    if userExists.rows_affected() == 1 {
-        return Ok(Json(response::GenericResponse {
-            success: false,
-            data: None,
-            errorMessage: Some(String::from("Username exists")),
-        }));
-    } else {
-        let salt_length = dotenvy::var("forum-register-salt-length").expect("FORUM-REGISTER-SALT-LENGTH must be set").parse::<i32>().unwrap();
-        let insertForumUserQuery = "INSERT INTO forum_users VALUES (NULL, ?, ?, ?, ?, 1, 1, 1);";
-        let random = rand::thread_rng();
-        let mut salt: Vec<u8> = Vec::new();
-        for i in 0..salt_length {
-            salt.push(random.clone().gen());
+    let mut user_is_logged_in = sqlx::query_as::<_, refresh_token_row>(forumUserIsLoggedInQuery)
+        .bind(&auth_header.body.user_id)
+        .fetch_one(&pool).await.unwrap();
+
+        if user_is_logged_in.count == 1 {
+            let mut tx = pool.begin().await.expect("begin tx");
+            let delete_specific_token_query = "DELETE FROM forum_refresh_tokens WHERE id = ?;";
+            let userExists = sqlx::query(delete_specific_token_query)
+                .bind(&user_is_logged_in.id)
+                .execute(&mut tx)
+                .await
+                .unwrap();
+            tx.commit().await.expect("tx commit");
+            // Set cookie to nothing
+            // Return success
+            return Ok(Json(response::GenericResponse {
+                success: true,
+                data: None,
+                errorMessage: None,
+            }));
+        } else {
+            // Delete all records in refresh token table
+            // Set cookie to nothing
+            Ok(Json(response::GenericResponse {
+                success: false,
+                data: None,
+                errorMessage: Some(String::from("User is not logged in")),
+            }))
         }
-        let its: std::num::NonZeroU32 = std::num::NonZeroU32::new(1000).unwrap();
-        // let hash = crypto::pbkdf2::pbkdf2();
-        let mut cred: [u8; 16] = [0u8; 16];
-        let hash = pbkdf2::derive(PBKDF2_ALG, its, &salt,
-            &auth_header.Password.as_bytes(), &mut cred);
-        // let db = SqlitePool::connect(DB_URL).await.unwrap();
-        let mut tx = pool.begin().await.expect("begin tx");
-        let userExists = sqlx::query(forumUserExistsQuery)
-            .bind(&time)
-            .bind(&auth_header.Username.to_lowercase())
-            .bind(&cred.to_ascii_lowercase())
-            .bind(&salt)
-            .execute(&mut tx)
-            .await
-            .unwrap();
-        tx.commit().await.expect("tx commit");
     }
 
-    Ok(Json(response::GenericResponse {
-        success: true,
-        data: None,
-        errorMessage: None,
-    }))
-}
 
