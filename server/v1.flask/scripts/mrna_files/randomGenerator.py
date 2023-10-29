c608a8868d8698991d1fed25da94ab73fd0d6fff
KooperL <kooper.lingohr@gmail.com>
2023-10-29 18:41:41 +1100

    Add sendMail script. Fix a python bug

--- server/v1.flask/scripts/mrna_files/randomGenerator.py
+++ server/v1.flask/scripts/mrna_files/randomGenerator.py
@@ -1,17 +1,19 @@
 import random
 from scripts.mrna_files.data import aminoAcids as amino_acid_dictionary
+from scripts.mrna_files.data import dna
+from scripts.mrna_files.data import rna
 
 
 def randomDNA(length: int) -> str:
   randomString = ''
-  validChars = ['A', 'T', 'G', 'C']
+  validChars = [x.get('symbol').upper() for x in dna()]
   for _ in range(0, int(length)):
     randomString = randomString + random.choice(validChars)
   return randomString
 
 def randomRNA(length: int) -> str:
   randomString = ''
-  validChars = ['A', 'U', 'G', 'C']
+  validChars = [x.get('symbol').upper() for x in rna()]
   for _ in range(0, int(length)):
     randomString = randomString + random.choice(validChars)
   return randomString
@@ -20,12 +22,12 @@ def randomProteins(length: int, single: bool) -> str:
   randomString = ''
   validChars = None
   if single == 1:
-    validChars = [x.symbol for x in amino_acid_dictionary]
+    validChars = [x.get('symbol').upper() for x in amino_acid_dictionary()]
     for _ in range(0, int(length)):
       randomString = randomString + random.choice(validChars)
     return randomString
   elif single == 0:
-    validChars = [x.three_letter_sybmol for x in amino_acid_dictionary]
+    validChars = [x.three_letter_sybmol for x in amino_acid_dictionary()]
     for _ in range(0, int(length)):
       randomString = randomString + f'{random.choice(validChars)},'
     randomString = randomString[:-1]
