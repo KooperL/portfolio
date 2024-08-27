915d1c22dc6c7aec4c4eaa38d90b899f52c54e71
KooperL <kooper.lingohr@gmail.com>
2024-08-27 19:07:00 +1000

    enhance: Polish content, fix footer buttons, add code project pages

---
+++ .svelte-kit/generated/client/nodes/9.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/projects/pocketbase-logging/+page.svelte";
---
+++ src/routes/projects/svelte-pocketbase-quickstart/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { pb } from "$lib/pocketbase";
+  import { onMount } from "svelte";
+  import { Card, Button, Skeleton } from "flowbite-svelte";
+  import { site } from "$lib/config";
+  import { ArrowRightToBracketOutline } from "flowbite-svelte-icons";
+
+  onMount(async () => {});
+</script>
+
+<div class="box-border p-8 w-full h-full">
+  <Card class="w-full max-w-full h-full bg-white/50">
+    <div class="flex flex-col space-y-4">
+      <span class="text-4xl font-bold" data-test="title">svelte-pocketbase-quickstart</span>
+      <div>
+        <p class=" font-bold">Boilerplate, distilled into it's most generic form.</p>
+        <p>Quickly get started with a Svelte project that uses Pocketbase as a backend service. This project is a great starting point for any Svelte project that needs a backend service.</p>
+      
+      </div>
+      <div class="flex space-x-4">
+        <Button href="/" class="w-48" variant="primary">back</Button>
+        <Button         href="https://github.com/KooperL/svelte-pocketbase-quickstart"
+        class="w-48" variant="primary"
+          ><ArrowRightToBracketOutline class="w-5 h-5 pr-2" />view code</Button
+        >
+      </div>
+    </div>
+  </Card>
+</div>
